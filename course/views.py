9b32aaa9217d7e9ba01aaf3f83ef0aab02397f33
firdavsDev <xackercoder@gmail.com>
2024-05-27 14:13:32 +0500

    added some changes

--- course/views.py
+++ course/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.db import models
 
 # Create your views here.
 from .models import Coures, CourseLesson
